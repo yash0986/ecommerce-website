f9389dfe93014b6de118849a48bc7fe6f4070a0d
yash0986 <116620799+yash0986@users.noreply.github.com>
2024-04-19 00:06:23 +0530

    updated server.js

--- server.js
+++ server.js
@@ -23,9 +23,10 @@ app.use(express.static(path.join(__dirname,'./client/build')));
 app.use("/api/v1/auth",authRoutes );
 app.use("/api/v1/category",categoryRoutes );
 app.use("/api/v1/product",productRoutes );
-app.use("*",function(req,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.html"));
-}) ;
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
 
 const PORT =  8080 ;
 
