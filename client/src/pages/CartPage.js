0ecbf85108adf7febf5c9d7695945fcc443c6af4
yash0986 <116620799+yash0986@users.noreply.github.com>
2024-04-19 00:56:58 +0530

    updated server.js

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -4,7 +4,7 @@ import { useCart } from "../context/cart";
 import { useAuth } from "../context/auth";
 import { useNavigate } from "react-router-dom";
 import DropIn from "braintree-web-drop-in-react";
-import { AiFillWarning } from "react-icons/ai";
+// import { AiFillWarning } from "react-icons/ai";
 import axios from "axios";
 import toast from "react-hot-toast";
 import "../styles/CartStyles.css";
