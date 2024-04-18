74a6a02af848416379e26274f5e7e37b2c097b4d
yash0986 <116620799+yash0986@users.noreply.github.com>
2024-04-18 23:26:07 +0530

    updated server.js

--- server.js
+++ server.js
@@ -5,21 +5,27 @@ import connectDB from './config/db.js';
 import morgan from 'morgan';
 import authRoutes from "./routers/authRoute.js"
 import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 import categoryRoutes from './routers/categoryRoutes.js'
 import productRoutes from './routers/productRoutes.js'
 connectDB();
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(__filename);
+
 const app = express()
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,'./client/build')));
 
 app.use("/api/v1/auth",authRoutes );
 app.use("/api/v1/category",categoryRoutes );
 app.use("/api/v1/product",productRoutes );
-app.get("/", (req,res)=>{
-    res.send("<h1>Hello world</h1>");
-});
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"));
+}) ;
 
 const PORT =  8080 ;
 
